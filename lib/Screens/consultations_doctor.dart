9a3394e19119973590dbedf8846ede78b6a23313
Chemsdine223 <126870359+Chemsdine223@users.noreply.github.com>
2023-06-23 00:45:53 +0000

    final touches

--- lib/Screens/consultations_doctor.dart
+++ lib/Screens/consultations_doctor.dart
@@ -16,6 +16,31 @@ class _DoctorAppointmentsPageState extends State<DoctorAppointmentsPage> {
   List<Map<String, dynamic>> allUsers = [];
   List<Map<String, dynamic>> foundUsers = [];
   Color defaultColor = Colors.white;
+  String? description;
+
+  Future<bool> addDescription(
+      dynamic consultationId, dynamic descriptions) async {
+    final url = '$baseUrl/user/description/$consultationId/';
+    final response = await http.post(Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(
+          {
+            "consultation_id": consultationId,
+            "description": descriptions,
+          },
+        ));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      print(response.body);
+      setState(() {
+        description = data['description'];
+      });
+      return true;
+    } else {
+      print(response.body);
+      return false;
+    }
+  }
 
   Future<void> acceptRequest(dynamic consultationId) async {
     final url = '$baseUrl/user/consultations/$consultationId/';
@@ -60,7 +85,7 @@ class _DoctorAppointmentsPageState extends State<DoctorAppointmentsPage> {
         ),
       );
       setState(() {
-        defaultColor = Colors.lightBlue[900]!;
+        defaultColor = Colors.red[600]!;
       });
     }
   }
@@ -121,6 +146,7 @@ class _DoctorAppointmentsPageState extends State<DoctorAppointmentsPage> {
             TextField(
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
+                hintText: 'Recherchez par nom du patient ...',
                 suffixIcon: Icon(Icons.search),
                 iconColor: Colors.lightBlue,
               ),
@@ -139,6 +165,95 @@ class _DoctorAppointmentsPageState extends State<DoctorAppointmentsPage> {
                   child: ListTile(
                     onTap: () {
                       // defaultColor = Colors.green;
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Container(
+                              height: MediaQuery.of(context).size.height / 2,
+                              width: MediaQuery.of(context).size.width,
+                              color: Colors.white,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Entrez une description',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  SizedBox(
+                                    // width: 200,
+                                    child: TextField(
+                                      decoration: const InputDecoration(
+                                          border: OutlineInputBorder(),
+                                          hintText: 'Entrez la description'),
+                                      controller: TextEditingController(
+                                        text: foundUsers[index]['description'],
+                                      ),
+                                      onChanged: (value) {
+                                        setState(() {
+                                          if (value.isNotEmpty) {
+                                            description = value;
+                                          } else {
+                                            description = foundUsers[index]
+                                                ['description'];
+                                          }
+                                        });
+                                      },
+                                      maxLines: null,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            actions: [
+                              MaterialButton(
+                                onPressed: () async {
+                                  final desc = await addDescription(
+                                    foundUsers[index]['id'],
+                                    description ??
+                                        foundUsers[index]['description'],
+                                  );
+                                  if (desc) {
+                                    setState(() {});
+                                    Navigator.pop(context);
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text(
+                                            'Description ajoutée avec succès'),
+                                      ),
+                                    );
+
+                                    // setState(() {
+                                    //   // description = description;
+                                    // });
+                                  } else {
+                                    Navigator.pop(context);
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(
+                                            content: Text(
+                                                'Ajout de description echoué')));
+                                  }
+
+                                  print(foundUsers[index]['id']);
+                                  // print(foundUsers[index]['consultation']);
+                                },
+                                color: Colors.lightBlue[900],
+                                child: const Text(
+                                  'Confirmer',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              )
+                              // IconButton(onPressed: (){}, icon: icon)
+                            ],
+                          );
+                        },
+                      );
                     },
                     tileColor: foundUsers[index]['status'] == 'accepted'
                         ? Colors.green
@@ -154,11 +269,27 @@ class _DoctorAppointmentsPageState extends State<DoctorAppointmentsPage> {
                         color: Colors.black,
                       ),
                     ),
-                    trailing: IconButton(
-                      onPressed: () {
-                        acceptRequest(foundUsers[index]['id']);
-                      },
-                      icon: const Icon(Icons.done),
+                    trailing: SizedBox(
+                      width: MediaQuery.of(context).size.width / 4,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              acceptRequest(foundUsers[index]['id']);
+                              // setState(() {
+                              //   defaultColor = Colors.greenAccent;
+                              // });
+                            },
+                            icon: const Icon(Icons.done),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              refuseRequest(foundUsers[index]['id']);
+                            },
+                            icon: const Icon(Icons.close),
+                          ),
+                        ],
+                      ),
                     ),
 
                     subtitle: Text(
